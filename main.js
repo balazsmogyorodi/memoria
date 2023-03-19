66625c599535a3ea15471e941522f8f807664742
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-03-19 11:14:36 +0100

    id megadas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //window.addEventListener("load", init);
-
+const kartyaHatat = "kepek/kartyahat.png";
 const list = [
     { kep: "kepek/banjo-and-kazooie.png", id: "1" },
     { kep: "kepek/banjo-and-kazooie.png",id: "1" },
@@ -13,34 +13,45 @@ const list = [
     { kep: "kepek/zero_suit_samus.png" , id: "5"},
     { kep: "kepek/steve.png" , id: "6"},
     { kep: "kepek/steve.png" , id: "6"}];
+    list.sort(function(){return 0.5 - Math.random()});
+    list.sort(function(){return 0.5 - Math.random()});
+
 const kivalasztot = [];
 const memoriaKartyak = [];
 
 $(window).ready(function () {
-    kartyak();
-    jatek.on('click')
+    const lefedettLapok = $("#jatek");
+    let tartalom = kartyak();
+    lefedettLapok.append(tartalom)
+    const lapForditas = $("#jatek img");
+    lapForditas.on("click", forditas);
+
+
+
+   
+   
 });
 
 
-
-
 function kartyak() {
-    const Element = $('#jatek');
     let kiiras = "";
-    list.sort(function(){return 0.5 - Math.random()});
         for (let index = 0; index < list.length; index++) {
-            kiiras += `<div class='kartya'> <img src='kepek/kartyahat.png' alt='memoria' class='kep' id=${list[index].id}> </div>`;
+            kiiras += `<div class='kartya'> <img src="${kartyaHatat}" alt='memoria' class='kep' id="${list[index].id}"> </div>`;
         }
-    Element.append(kiiras);
-    console.log(Element);
+    console.log(kiiras);
+    return kiiras;
+}
+
+
+function forditas() {
+    const aktkep = event.target;
+    console.log(aktkep.id)
+
+
 
 }
 
-function jatek() {
 
 
-
-
-}
 
 
